7080c8f94d1d6016948ed0f9dfeb29380cb15b45
18 <111544899+eightween@users.noreply.github.com>
2024-02-23 19:24:39 -0500

    chore: update landing contents

--- js/track.js
+++ js/track.js
@@ -1,7 +1,7 @@
 function track() {
     (function () {
         var on = addEventListener, $ = function (q) { return document.querySelector(q); }, $$ = function (q) { return document.querySelectorAll(q); }, $body = document.body, $inner = $('.inner'), client = (function () {
-            var o = { browser: 'other', browserVersion: 0, os: 'other', osVersion: 0, mobile: false, canUse: null, flags: { lsdUnits: false, }, }, ua = navigator.userAgent, a, i; a = [['firefox', /Firefox\/([0-9\.]+)/], ['edge', /Edge\/([0-9\.]+)/], ['safari', /Version\/([0-9\.]+).+Safari/], ['chrome', /Chrome\/([0-9\.]+)/], ['chrome', /CriOS\/([0-9\.]+)/], ['ie', /Trident\/.+rv:([0-9]+)/]]; for (i = 0; i < a.length; i++) { if (ua.match(a[i][1])) { o.browser = a[i][0]; o.browserVersion = parseFloat(RegExp.$1); break; } } a = [['ios', /([0-9_]+) like Mac OS X/, function (v) { return v.replace('_', '.').replace('_', ''); } ], ['ios', /CPU like Mac OS X/, function (v) { return 0; } ], ['ios', /iPad; CPU/, function (v) { return 0; } ], ['android', /Android ([0-9\.]+)/, null], ['mac', /Macintosh.+Mac OS X ([0-9_]+)/, function (v) { return v.replace('_', '.').replace('_', ''); } ], ['windows', /Windows NT ([0-9\.]+)/, null], ['undefined', /Undefined/, null],]; for (i = 0; i < a.length; i++) { if (ua.match(a[i][1])) { o.os = a[i][0]; o.osVersion = parseFloat(a[i][2] ? (a[i][2])(RegExp.$1) : RegExp.$1); break; } } if (o.os == 'mac' && ('ontouchstart' in window) && ((screen.width == 1024 && screen.height == 1366) || (screen.width == 834 && screen.height == 1112) || (screen.width == 810 && screen.height == 1080) || (screen.width == 768 && screen.height == 1024)))
+            var o = { browser: 'other', browserVersion: 0, os: 'other', osVersion: 0, mobile: false, canUse: null, flags: { lsdUnits: false, }, }, ua = navigator.userAgent, a, i; a = [['firefox', /Firefox\/([0-9\.]+)/], ['edge', /Edge\/([0-9\.]+)/], ['safari', /Version\/([0-9\.]+).+Safari/], ['chrome', /Chrome\/([0-9\.]+)/], ['chrome', /CriOS\/([0-9\.]+)/], ['ie', /Trident\/.+rv:([0-9]+)/]]; for (i = 0; i < a.length; i++) { if (ua.match(a[i][1])) { o.browser = a[i][0]; o.browserVersion = parseFloat(RegExp.$1); break; } } a = [['ios', /([0-9_]+) like Mac OS X/, function (v) { return v.replace('_', '.').replace('_', ''); }], ['ios', /CPU like Mac OS X/, function (v) { return 0; }], ['ios', /iPad; CPU/, function (v) { return 0; }], ['android', /Android ([0-9\.]+)/, null], ['mac', /Macintosh.+Mac OS X ([0-9_]+)/, function (v) { return v.replace('_', '.').replace('_', ''); }], ['windows', /Windows NT ([0-9\.]+)/, null], ['undefined', /Undefined/, null],]; for (i = 0; i < a.length; i++) { if (ua.match(a[i][1])) { o.os = a[i][0]; o.osVersion = parseFloat(a[i][2] ? (a[i][2])(RegExp.$1) : RegExp.$1); break; } } if (o.os == 'mac' && ('ontouchstart' in window) && ((screen.width == 1024 && screen.height == 1366) || (screen.width == 834 && screen.height == 1112) || (screen.width == 810 && screen.height == 1080) || (screen.width == 768 && screen.height == 1024)))
                 o.os = 'ios'; o.mobile = (o.os == 'android' || o.os == 'ios'); var _canUse = document.createElement('div'); o.canUse = function (property, value) {
                     var style; style = _canUse.style; if (!(property in style))
                         return false; if (typeof value !== 'undefined') {
@@ -9,7 +9,7 @@ function track() {
                                 return false;
                         } return true;
                 }; o.flags.lsdUnits = o.canUse('width', '100dvw'); return o;
-        } ()), trigger = function (t) { dispatchEvent(new Event(t)); }, cssRules = function (selectorText) {
+        }()), trigger = function (t) { dispatchEvent(new Event(t)); }, cssRules = function (selectorText) {
             var ss = document.styleSheets, a = [], f = function (s) {
                 var r = s.cssRules, i; for (i = 0; i < r.length; i++) {
                     if (r[i] instanceof CSSMediaRule && matchMedia(r[i].conditionText).matches)
@@ -26,13 +26,13 @@ function track() {
         }, scrollToElement = function (e, style, duration) {
             var y, cy, dy, start, easing, offset, f; if (!e)
                 y = 0; else {
-                    offset = (e.dataset.scrollOffset ? parseInt(e.dataset.scrollOffset) : 0) * parseFloat(getComputedStyle(document.documentElement).fontSize); switch (e.dataset.scrollBehavior ? e.dataset.scrollBehavior : 'default') {
-                        case 'default': default: y = e.offsetTop + offset; break; case 'center': if (e.offsetHeight < window.innerHeight)
-                            y = e.offsetTop - ((window.innerHeight - e.offsetHeight) / 2) + offset; else
-                            y = e.offsetTop - offset; break; case 'previous': if (e.previousElementSibling)
-                                y = e.previousElementSibling.offsetTop + e.previousElementSibling.offsetHeight + offset; else
-                                y = e.offsetTop + offset; break;
-                    }
+                offset = (e.dataset.scrollOffset ? parseInt(e.dataset.scrollOffset) : 0) * parseFloat(getComputedStyle(document.documentElement).fontSize); switch (e.dataset.scrollBehavior ? e.dataset.scrollBehavior : 'default') {
+                    case 'default': default: y = e.offsetTop + offset; break; case 'center': if (e.offsetHeight < window.innerHeight)
+                        y = e.offsetTop - ((window.innerHeight - e.offsetHeight) / 2) + offset; else
+                        y = e.offsetTop - offset; break; case 'previous': if (e.previousElementSibling)
+                            y = e.previousElementSibling.offsetTop + e.previousElementSibling.offsetHeight + offset; else
+                            y = e.offsetTop + offset; break;
+                }
             } if (!style)
                 style = 'smooth'; if (!duration)
                 duration = 750; if (style == 'instant') { window.scrollTo(0, y); return; } start = Date.now(); cy = window.scrollY; dy = y - cy; switch (style) { case 'linear': easing = function (t) { return t; }; break; case 'smooth': easing = function (t) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; }; break; } f = function () {
